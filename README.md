8d22df595f6f306d7a4bf01e5fb583d0a5469b4a
chaonan99 <chaonan99@cs.unc.edu>
2018-09-16 01:07:10 -0400

    :sparkles: Baseline

--- README.md
+++ README.md
@@ -2,7 +2,31 @@
 
 * Our Kaggle competition repo for [What's Cooking](https://www.kaggle.com/c/whats-cooking-kernels-only)
 
+## Statistics
+* #Train: 39774
+* #Test: 9944
+* #Class: 20
+* #Train ingredients: 6714
+* #Test ingredients: 4484
+* Train, test ingredient overlaps: 4061 (non-overlap 4484-4061=423)
 
 ## TODOs
 - [ ] Tf-idf baseline
+- [ ] Dataset bias
 - [ ] 2-gram
+
+## Performance
+On dev:
+* Baseline: 0.6959975864843122
+* Direct tf-idf: 0.7338093322606597
+* Binary tf-idf: 0.7389380530973452
+* SVC C=1: 0.6562751407884151
+* SVC C=200: 0.7578439259855189
+* SVC C=300: 0.7687047465808527
+* SVC C=500: 0.7763475462590507
+* SVC C=1000: 0.7824818986323411
+* SVC C=1000, balanced: 0.7539219629927595
+* SVC C=100, balanced: 0.7543242156074015
+* SVC C=100, coef0=1, balanced: 0.7389380530973452
+* SVC C=100, coef0=1, class_weight=None: 0.7543242156074015
+
--- src/tf_idf_rank.py
+++ src/tf_idf_rank.py
@@ -5,42 +5,76 @@
 import json
 
 import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import svm
-
-from config import train_data, test_data
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import accuracy_score
 
 
 __author__ = ['chaonan99']
 
 
-def main():
+train_data = '../input/train.json'
+test_data = '../input/test.json'
+
+
+def run_pipeline(submission=False):
     np.random.seed(42)
     get_ingredients = lambda x: list(map(lambda x: x['ingredients'], x))
     get_cuisine = lambda x: list(map(lambda x: x['cuisine'], x))
-    ingredient_format = lambda x: ' '.join([i.replace(' ', '_') for i in x])
+    ingredient_format = lambda x: ' '.join(x)
+    # ingredient_format = lambda x: ' '.join([i.replace(' ', '_') for i in x])
 
     train_content = json.load(open(train_data))
     test_content = json.load(open(test_data))
     np.random.shuffle(train_content)
-    train_content, dev_content = train_test_split(train_content)
+    if not submission:
+        train_content, dev_content = train_test_split(train_content,
+                                                      test_size=0.25)
 
-    tfidf = TfidfVectorizer()
-    X_tr = tfidf.fit_transform(map(ingredient_format, get_ingredients(train_content)))
-    X_dev = tfidf.transform(map(ingredient_format, get_ingredients(dev_content)))
-    # X_te = tfidf.transform(map(ingredient_format, get_ingredients(test_content)))
+    tfidf = TfidfVectorizer(binary=True)
+    X_tr = tfidf.fit_transform(map(ingredient_format,
+                                   get_ingredients(train_content)))
+    X_te = tfidf.transform(map(ingredient_format,
+                               get_ingredients(test_content)))
+    if not submission:
+        X_dev = tfidf.transform(map(ingredient_format,
+                                    get_ingredients(dev_content)))
 
     ## Nearest neighbor
     le = preprocessing.LabelEncoder()
     y_tr = le.fit_transform(get_cuisine(train_content))
-    y_dev = le.transform(get_cuisine(dev_content))
+    if not submission:
+        y_dev = le.transform(get_cuisine(dev_content))
 
-    from IPython import embed; embed(); import os; os._exit(1)
-    clf = svm.SVC(C=100, kernel='rbf', verbose=True)
+    # clf = svm.SVC(C=1000, kernel='rbf', verbose=True)
+    # clf = svm.SVC(C=1000, kernel='rbf', class_weight='balanced', verbose=True)
+    clf = svm.SVC(C=1000,
+                  kernel='rbf',
+                  # coef0=1,
+                  # class_weight=None,
+                  verbose=True)
+    clf = OneVsRestClassifier(clf)
     clf.fit(X_tr, y_tr)
-    clf.predict(X_dev)
+
+    if not submission:
+        y_dev_pred = clf.predict(X_dev)
+        print("Accuracy:", accuracy_score(y_dev, y_dev_pred))
+    else:
+        y_te = clf.predict(X_te)
+        test_label_pred = le.inverse_transform(y_te)
+
+        test_id = list(map(lambda x: x['id'], test_content))
+        sub = pd.DataFrame({'id': test_id, 'cuisine': test_label_pred},
+                           columns=['id', 'cuisine'])
+        sub.to_csv('../dump/svm_output.csv', index=False)
+
+
+def main():
+    run_pipeline(submission=False)
 
 
 if __name__ == '__main__':
